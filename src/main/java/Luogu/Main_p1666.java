9487fa7719f3c7fb95921855c9b9b761522b5f7c
yiyi <2713088477@qq.com>
2026-05-23 22:03:31 +0800

    2026-05-23

--- src/main/java/Luogu/Main_p1666.java
+++ src/main/java/Luogu/Main_p1666.java
@@ -3,11 +3,12 @@ package Luogu;
 import java.io.*;
 import java.util.Arrays;
 
+//测试链接: https://www.luogu.com.cn/problem/P1666
 //TODO:这道题有问题
 public class Main_p1666 {
 	public static int MAX_LEN = 51;
 	public static int[][] nodes = new int[MAX_LEN][26];
-	public static int[] end = new int[MAX_LEN];
+	public static int[] pass = new int[MAX_LEN];
 	public static int cnt = 1;
 	
 	public static void main(String[] args) throws IOException {
@@ -19,14 +20,13 @@ public class Main_p1666 {
 			n = (int)in.nval;
 			int ans = (int)Math.pow(2, n);
 			build();
-			System.out.println("ans="+ans);
 			String cur;
 			for(int i=0,sameCount;i<n;i++) {
 				in.nextToken();
 				cur = in.sval;
 				sameCount = addTree(cur);
-				System.out.println(String.format("%s这个字符串重复的次数:%d", cur,sameCount));
-				System.out.println(String.format("减少的答案数:%d", (int)Math.pow(2, n-2)*sameCount));
+				//System.out.println(String.format("%s 有%d 个前缀相同的",cur,sameCount));
+
 				ans -= Math.pow(2, n-2)*sameCount;
 			}
 			out.println(ans);
@@ -37,27 +37,25 @@ public class Main_p1666 {
 		
 	}
 	public static void build() {
+		cnt = 1;
 		for(int[] t:nodes) {
-			Arrays.fill(t, -1);
+			Arrays.fill(t, 0);
 		}
-		Arrays.fill(end, 0);
+		Arrays.fill(pass, 0);
 		
 	}
 	public static int addTree(String word) {
-		int sameCount = 0;
 		int current = 0;
+		pass[current]++;
 		for(int i=0,next;i<word.length();i++) {
 			next = word.charAt(i)-'a';
-			if(nodes[current][next] == -1) {
+			if(nodes[current][next] == 0) {
 				nodes[current][next] = cnt++;
 			}
 			current = nodes[current][next];
-			if(end[current] != 0) {
-				sameCount++;
-			}
+			pass[current]++;
 		}
-		end[current]++;
-		return sameCount;
+		return pass[current]-1;
 		
 	}
 
