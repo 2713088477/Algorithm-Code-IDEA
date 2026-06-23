8c67bd0559a7016029518f3ef2e9a800a0807c2d
yiyi <2713088477@qq.com>
2026-06-23 22:02:07 +0800

    2026-06-23

--- src/main/java/ZuoVideo64/Code01_Solution743.java
+++ src/main/java/ZuoVideo64/Code01_Solution743.java
@@ -6,7 +6,15 @@ import java.util.PriorityQueue;
 
 //测试链接:https://leetcode.cn/problems/network-delay-time/description/
 public class Code01_Solution743 {
-    public int networkDelayTime(int[][] times, int n, int k) {
+
+    /**
+     * 普通堆实现
+     * @param times
+     * @param n
+     * @param k
+     * @return
+     */
+    public int networkDelayTime1(int[][] times, int n, int k) {
         //邻接表建图
         ArrayList<ArrayList<int[]>> edges = new ArrayList<>();
         for(int i=0;i<=n;i++){
@@ -45,4 +53,66 @@ public class Code01_Solution743 {
         }
         return ans;
     }
+
+
+    /**
+     * 反向索引堆(极致)
+     * @param times
+     * @param n
+     * @param k
+     * @return
+     */
+    public int networkDelayTime2(int[][] times, int n, int k) {
+        build(n);
+        for(int[] time:times){
+            int from = time[0],to = time[1],value = time[2];
+            addEdge(from,to,value);
+        }
+
+
+    }
+    public static int MAX_N = 101;
+    public static int MAX_M = 6001;
+
+    public static int[] head = new int[MAX_N];
+    public static int[] next = new int[MAX_M];
+    public static int[] to = new int[MAX_M];
+    public static int[] weight = new int[MAX_M];
+    public static int edgeId;
+
+    public static int[] minHeap = new int[MAX_N];
+    public static int[] where = new int[MAX_N];
+    public static int heapSize;
+
+    public static int[] distance = new int[MAX_N];
+    public static void build(int n){
+        edgeId = 1;
+        Arrays.fill(head,1,n+1,0);
+        Arrays.fill(where,1,n+1,-1);
+        Arrays.fill(distance,1,n+1,Integer.MAX_VALUE);
+        heapSize = 0;
+    }
+    public static void addEdge(int fromNode,int toNode,int value){
+        next[edgeId] = head[fromNode];
+        to[edgeId] = toNode;
+        weight[edgeId] = value;
+        head[fromNode] = edgeId++;
+    }
+    public static void addOrUpdateOrIgnore(int nodeId,int dis){
+        if(where[nodeId] == -1){
+            heapInsert(nodeId,dis);
+        }
+    }
+    public static void heapInsert(int nodeId,int dis){
+        int heapIndex = heapSize++;
+        where[nodeId] = heapIndex; //定义这个nodeId
+        minHeap[heapIndex] = dis;
+        while(minHeap[(heapIndex-1)/2] < minHeap[heapIndex]){
+
+        }
+    }
+    public static void swap(int heapIndex1,int heapIndex2){
+
+    }
+
 }
